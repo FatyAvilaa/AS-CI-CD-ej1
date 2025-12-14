af7a189afcd392cd0ace7929552313593a85225f
FatyAvilaa <84937591+FatyAvilaa@users.noreply.github.com>
2025-12-14 23:47:26 +0100

    Add test for increment_balance function
    Added a test for the increment_balance function.

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-from wallet import Wallet
+from wallet import Wallet, increment_balance
+
 
 def test_getbalance():
     obj = Wallet(0)
@@ -14,3 +15,6 @@ def test_setbalance():
     obj = Wallet(0)
     obj.set_balance(40)
     assert obj.get_balance() == 40
+    
+def test_increment_balance():
+    assert increment_balance(500) == 1500
